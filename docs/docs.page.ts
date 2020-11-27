c7449dc0848f570d96bcaf686545b28d488c15f4
Romuald Brillout <github.com@brillout.com>
2020-11-27 20:49:34 +0100

    minor

--- docs/docs.page.ts
+++ docs/docs.page.ts
@@ -3,4 +3,6 @@ import { DocsPage } from "./DocsPage";
 export default {
   route: "/docs",
   view: DocsPage,
+  renderHtmlAtBuildTime: true,
+  renderToDom: false,
 };
--- landing/landing.page.ts
+++ landing/landing.page.ts
@@ -3,4 +3,6 @@ import { LandingPage } from "./LandingPage";
 export default {
   route: "/",
   view: LandingPage,
+  renderHtmlAtBuildTime: true,
+  renderToDom: false,
 };
